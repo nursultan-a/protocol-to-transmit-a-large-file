c7194821a14ac969f501bcee370fffd0ab7aa923
e1906841 <e1906841@r2.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-18 13:51:44 -0800

    R2 ready 90%

--- example.py
+++ example.py
@@ -7,58 +7,68 @@ import time
 ServerAddress = ("", 5050)
 
 ClientAdress = {
+        "s"  :"10.10.2.2",
         "r1" :"10.10.8.1",
         "r3" :"10.10.6.2",
-        "s"  :"10.10.2.2",
         "d"  :"10.10.5.2"
 
         }
 initiate = True
 ThreadList = []
 ThreadCount = 10
+bufferSize = 1024
+
+
+def get_time():
+      return int(round(time.time() * 1000))
+
+def Connect2Server(address):
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    serverAddressPort = (address, 5050)
+
+    msg = "START_R1*"+str(get_time())+"*NA"
+    bytesToSend = str.encode(msg)
+
+
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+    msg = address +" : "+str(repr(msgFromServer[0])[2:-1])
+
+    print(msg)
+
+
+
 class UDPRequestHandler(socketserver.DatagramRequestHandler):
-
-    def get_time():
-          return int(round(time.time() * 1000))
-
-    def Connect2Server(adress):
-        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        serverAdressPort = (address, 5050)
-
-        msg = "S*"+str(get_time)+"*"
-        bytesToSend = str.encode(msg)
-
-
-        UDPClientSocket.sendto(bytesToSend, serverAsddresPort)
-
-        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-        msg = "Message from Server {}".format(msgFromServer[0])
-
-             
-
-        print(msg)
     #override
     def handle(self):
-        datagram = str(self.rfile.readline().strip())
+        datagram = str(repr(self.rfile.readline().strip())[2:-1])
         address = "{}".format(self.client_address[0])
 
-        #print("[" + address + "] : "+datagram)
-        if(initiate and address == ClientAdress["s"]):
+        print("[" + address + "] : "+datagram)
+        global initiate
+        global ThreadCount
+        global ThreadList
+
+        # initiated from S => send discovery message to : R1, R2, R3, S, D
+        if(initiate == True and address == ClientAdress["s"]):
+            
+            initiate = False
             for key in ClientAdress:
                 for index in range(ThreadCount):
                     ThreadInstance = threading.Thread(target=Connect2Server(ClientAdress[key]))
                     ThreadList.append(ThreadInstance)
                     ThreadInstance.start()
-
                 #main thread to wait till all connection threads are complete
                 for index in range(ThreadCount):
                     ThreadList[index].join()
-
-
-        print("Thread Name: {}".format(threading.current_thread().name))
-
-        self.wfile.write("Message from R2! Hello r1".encode())
+        
+        # respond to requested UDP messages
+        else:
+            #print("Thread Name: {}".format(threading.current_thread().name))
+            ACK = "ACK_R2*"+datagram
+            self.wfile.write(ACK.encode())
 
 
 UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress,UDPRequestHandler)
