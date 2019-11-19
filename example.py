b2149b91660878b80f46a56f4cf46a00670735b2
e1906841 <e1906841@r3.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-19 10:40:37 -0800

    termination of server and rtt of each neighbour hosts coded:

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ import time
 ServerAddress = ("", 5050)
 
 ClientAdress = {
-        "s"  :"10.10.3.1",
         "r2" :"10.10.6.1",
+        "s"  :"10.10.3.1",
         "d"  :"10.10.7.1"
         }
 
@@ -50,21 +50,33 @@ def Connect2Server(address, msg_id):
 
     print(msg)
     end_time = get_time()
-    start_time =(msgFromServer.split("*"))
-    for i in start_time:
-        print(i)
-#    print(start_time[0])
-    #print("start time: "+str(start_time)+"   end time: "+ str(end_time_))
-#    difference = end_time - start_time
 
-#    host = msgFromServer.split("*")[0]
+    start_time =int(msgFromServer.split("*")[2])
+    visited_host = msgFromServer.split("*")[0].split("_")[1].lower()
+
     global total_time_s
-#    if host == "START_S":
-#        total_time_s += difference
-#        terminate -= 1
+    global total_time_r2
+    global total_time_d
+
+    if len(msgFromServer.split("*")) >= 4:
+        difference = end_time - start_time
+        print(str(end_time)+" - "+str(start_time)+"="+str(difference))
+        terminate -= 1
+        if(visited_host == "s"):
+            total_time_s += difference
+        elif(visited_host == "r2"):
+            total_time_r2 += difference
+        elif(visited_host == "d"):
+            total_time_d += difference
+    print("CONDITION-----------------------------------------"+str(terminate))
 
 
 
+def close_server(ThreadList):
+    for t in ThreadList:
+        if t.is_alive() == True:
+            return False
+    return True
 
 class UDPRequestHandler(socketserver.DatagramRequestHandler):
     #override
@@ -76,7 +88,13 @@ class UDPRequestHandler(socketserver.DatagramRequestHandler):
         global initiate
         global ThreadCount
         global ThreadList
+        global terminate
 
+        global total_time_s
+        global total_time_r2
+        global total_time_d
+
+        global UDPServerObject
         # initiated from R2 => send discovery message to : S, R2, D
         if(initiate == True and address == ClientAdress["s"]):
             
@@ -89,13 +107,23 @@ class UDPRequestHandler(socketserver.DatagramRequestHandler):
                 #main thread to wait till all connection threads are complete
                 for index in range(ThreadCount):
                     ThreadList[index].join()
-        
+                        
         # respond to requested UDP messages
         else:
             #print("Thread Name: {}".format(threading.current_thread().name))
+
+            terminate -= 1
+            #print("condition -----------------"+ str(terminate))
             ACK = "ACK_R3*"+datagram
             self.wfile.write(ACK.encode())
+            if(terminate == 0):
+                print("terminating server from ACK side")
 
+                UDPServerObject.shutdown()
+                rtt_s = total_time_s/ThreadCount
+                rtt_r2 = total_time_r2/ThreadCount
+                rtt_d = total_time_d/ThreadCount
+                print ("rtt for s: "+str(rtt_s)+" rtt for r2: "+str(rtt_r2)+" rtt for d: "+str(rtt_d))
 UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress,UDPRequestHandler)
 
 UDPServerObject.serve_forever()
