2465016e7583e720d9e1605a0e2d044e38749e79
e1906841 <e1906841@r2.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-20 08:36:57 -0800

    RTT done

--- r2.py
+++ r2.py
@@ -1,91 +1,157 @@
-import socket
-import sys
-from _thread import *
+import socketserver
 import threading
-
-print_lock = threading.Lock()
-
-#thread function
-def threaded(c, address, value):
-    while True:
-        # data received from client
-        data = c.recv(1024)
-        if not data:
-            print('No data')
-
-            # lock released on exit
-            print_lock.release()
-            break
-        print(data)
-        # reverse the given string from client
-        data = data[::-1]
-
-        # send back
-        c.send(data)
-
-    # close connection
-    c.close()
-
-def Main():
-    ####### get ip adress of connected hosts #####################
-    try:
-        r1_ip = socket.gethostbyname('r1')
-        print("r1 ip: %s" %(r1_ip))
-    except socket.gaierror:
-        print("there was an error on resolving the r1")
-        sys.exit()
+import socket
+import time
 
 
-    try:
-        d_ip = socket.gethostbyname('d')
-        print("d ip: %s" %(d_ip))
-    except socket.gaierror:
-        print("there was an error on resolving the d")
-        sys.exit()
+ServerAddress = ("", 5050)
 
-    try:
-        r3_ip = socket.gethostbyname('r3')
-        print("r1 ip: %s" %(r3_ip))
-    except socket.gaierror:
-        print("there was an error on resolving the r3")
-        sys.exit()
+ClientAddress = {
+        "s"  :"10.10.2.2",
+        "r3" :"10.10.6.2",
+        "d"  :"10.10.5.2",
+        "r1" :"10.10.8.1"
 
+        }
+initiate = True
+ThreadList = []
+ThreadCount = 1000
+bufferSize = 1024
 
-    try:
-        s_ip = socket.gethostbyname('s')
-        print("s ip: %s" %(s_ip))
-    except socket.gaierror:
-        print("there was an error on resolving the s")
-        sys.exit()
-    ##############################################################
-    port = 12345
-    ########### create client and server socet for r1 ############
-    try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-        print("server_socket successfully created")
+rtt_s  = 0
+rtt_r1 = 0
+rtt_r2 = 0
+rtt_d  = 0
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-        print("client_socket successfully created")
-    except socket.error as err:
-        print("socket creation failed with error %s" %(err))
+total_time_s  = 0
+total_time_r1 = 0
+total_time_r3 = 0
+total_time_d  = 0
+
+terminate = ThreadCount*8
 
 
-    server_socket.bind(("", port))
-    print("server_socket binded to %s" %(port))
+def get_time():
+      return int(round(time.time() * 1000))
 
-    while(True):
-        value, address = server_socket.recvfrom(1024)
-        c = server_socket.recvfrom(1024)
+def Connect2Server(address, msg_id):
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    serverAddressPort = (address, 5050)
 
-        print_lock.acquire()
-
-        print('[',address,'] : ',value)
-
-        # start new thread
-        start_new_thread(threaded,(c, value, address))
-        server_socket.sendto("hello from r2".encode(), (r1_ip,port))
+    msg = "START_R2*"+str(get_time())+"*NA*"+str(msg_id)
+    bytesToSend = str.encode(msg)
 
 
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-if __name__ == '__main__':
-    Main()
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msgFromServer = str(repr(msgFromServer[0])[2:-1])
+
+    msg = "["+address +"] : "+ msgFromServer
+
+    print(msg)
+
+    end_time = get_time()
+
+
+    start_time = int(msgFromServer.split("*")[2])
+    visited_host = msgFromServer.split("*")[0].split("_")[1].lower()
+
+    global total_time_s
+    global total_time_r1
+    global total_time_r3
+    global total_time_d
+
+    global terminate
+
+    if len(msgFromServer.split("*")) >= 4:
+        difference = end_time - start_time
+        terminate -= 1
+        print("condition----------------------------"+str(terminate))
+
+        if visited_host == "s":
+            total_time_s += difference
+        elif visited_host == "r1":
+            total_time_r1 += difference
+        elif visited_host == "r3":
+            total_time_r3 += difference
+        elif visited_host == "d":
+            total_time_d += difference
+        if(terminate == 0):
+            print("terminating server")
+            UDPServerObject.shutdown()
+            
+            rtt_s = total_time_s/ThreadCount
+            rtt_r1 = total_time_r1/ThreadCount
+            rtt_r3 = total_time_r3/ThreadCount
+            rtt_d = total_time_d/ThreadCount
+
+            f = open("link_costs.txt", "w+")
+            f.write(str(rtt_s) +", "+str(rtt_r1)+", "+str(rtt_r3)+", "+str(rtt_d))
+            f.close()
+class UDPRequestHandler(socketserver.DatagramRequestHandler):
+    #override
+    def handle(self):
+        datagram = str(repr(self.rfile.readline().strip())[2:-1])
+        address = "{}".format(self.client_address[0])
+
+        print("[" + address + "] : "+datagram)
+    
+        global initiate
+        global ThreadCount
+        global ThreadList
+        global terminate
+
+        global total_time_s
+        global total_time_r1
+        global total_time_r3
+        global total_time_d
+
+        global UDPServerObject
+
+        # initiated from S =>initiate D and send discovery message to : R1, R2, R3, S, D
+        if(initiate == True and address == ClientAddress["s"]):
+
+            # initiate D
+            d_init_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+            msg = "initiate message from R2 :)"
+            d_init_socket.sendto(str.encode(msg+" start working D"), (ClientAddress["d"], 5050))
+
+            initiate = False
+
+            # send discovery message to near hosts
+            for key in ClientAddress:
+                for index in range(ThreadCount):
+                    ThreadInstance = threading.Thread(target=Connect2Server(ClientAddress[key], index))
+                    ThreadList.append(ThreadInstance)
+                    ThreadInstance.start()
+                #main thread to wait till all connection threads are complete
+                for index in range(ThreadCount):
+                    ThreadList[index].join()
+        
+        # respond to requested UDP messages
+        else:
+            #print("Thread Name: {}".format(threading.current_thread().name))
+            terminate -= 1
+            ACK = "ACK_R2*"+datagram
+            self.wfile.write(ACK.encode())
+
+            print("condition-ack--------------------"+str(terminate))
+            if(terminate == 0):
+                print("terminating server")
+                UDPServerObject.shutdown()
+                
+                rtt_s = total_time_s/ThreadCount
+                rtt_r1 = total_time_r1/ThreadCount
+                rtt_r3 = total_time_r3/ThreadCount
+                rtt_d = total_time_d/ThreadCount
+
+                f = open("link_costs.txt", "w+")
+                f.write(str(rtt_s) +", "+str(rtt_r1)+", "+str(rtt_r3)+", "+str(rtt_d))
+                f.close()
+
+
+
+UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress,UDPRequestHandler)
+
+UDPServerObject.serve_forever()
