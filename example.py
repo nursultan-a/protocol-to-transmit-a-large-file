8124b592879f7427f106e3d7c6b2af179beaa2fc
e1906841 <e1906841@r2.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-19 19:46:06 -0800

    done! rename and rechack

--- example.py
+++ example.py
@@ -8,15 +8,27 @@ ServerAddress = ("", 5050)
 
 ClientAddress = {
         "s"  :"10.10.2.2",
-        "r1" :"10.10.8.1",
         "r3" :"10.10.6.2",
-        "d"  :"10.10.5.2"
+        "d"  :"10.10.5.2",
+        "r1" :"10.10.8.1"
 
         }
 initiate = True
 ThreadList = []
-ThreadCount = 10
+ThreadCount = 1000
 bufferSize = 1024
+
+rtt_s  = 0
+rtt_r1 = 0
+rtt_r2 = 0
+rtt_d  = 0
+
+total_time_s  = 0
+total_time_r1 = 0
+total_time_r3 = 0
+total_time_d  = 0
+
+terminate = ThreadCount*8
 
 
 def get_time():
@@ -33,13 +45,50 @@ def Connect2Server(address, msg_id):
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msgFromServer = str(repr(msgFromServer[0])[2:-1])
 
-    msg = "["+address +"] : "+str(repr(msgFromServer[0])[2:-1])
+    msg = "["+address +"] : "+ msgFromServer
 
     print(msg)
 
+    end_time = get_time()
 
 
+    start_time = int(msgFromServer.split("*")[2])
+    visited_host = msgFromServer.split("*")[0].split("_")[1].lower()
+
+    global total_time_s
+    global total_time_r1
+    global total_time_r3
+    global total_time_d
+
+    global terminate
+
+    if len(msgFromServer.split("*")) >= 4:
+        difference = end_time - start_time
+        terminate -= 1
+        print("condition----------------------------"+str(terminate))
+
+        if visited_host == "s":
+            total_time_s += difference
+        elif visited_host == "r1":
+            total_time_r1 += difference
+        elif visited_host == "r3":
+            total_time_r3 += difference
+        elif visited_host == "d":
+            total_time_d += difference
+        if(terminate == 0):
+            print("terminating server")
+            UDPServerObject.shutdown()
+            
+            rtt_s = total_time_s/ThreadCount
+            rtt_r1 = total_time_r1/ThreadCount
+            rtt_r3 = total_time_r3/ThreadCount
+            rtt_d = total_time_d/ThreadCount
+
+            f = open("link_costs.txt", "w+")
+            f.write(str(rtt_s) +", "+str(rtt_r1)+", "+str(rtt_r3)+", "+str(rtt_d))
+            f.close()
 class UDPRequestHandler(socketserver.DatagramRequestHandler):
     #override
     def handle(self):
@@ -47,9 +96,18 @@ class UDPRequestHandler(socketserver.DatagramRequestHandler):
         address = "{}".format(self.client_address[0])
 
         print("[" + address + "] : "+datagram)
+    
         global initiate
         global ThreadCount
         global ThreadList
+        global terminate
+
+        global total_time_s
+        global total_time_r1
+        global total_time_r3
+        global total_time_d
+
+        global UDPServerObject
 
         # initiated from S =>initiate D and send discovery message to : R1, R2, R3, S, D
         if(initiate == True and address == ClientAddress["s"]):
@@ -74,8 +132,24 @@ class UDPRequestHandler(socketserver.DatagramRequestHandler):
         # respond to requested UDP messages
         else:
             #print("Thread Name: {}".format(threading.current_thread().name))
+            terminate -= 1
             ACK = "ACK_R2*"+datagram
             self.wfile.write(ACK.encode())
+
+            print("condition-ack--------------------"+str(terminate))
+            if(terminate == 0):
+                print("terminating server")
+                UDPServerObject.shutdown()
+                
+                rtt_s = total_time_s/ThreadCount
+                rtt_r1 = total_time_r1/ThreadCount
+                rtt_r3 = total_time_r3/ThreadCount
+                rtt_d = total_time_d/ThreadCount
+
+                f = open("link_costs.txt", "w+")
+                f.write(str(rtt_s) +", "+str(rtt_r1)+", "+str(rtt_r3)+", "+str(rtt_d))
+                f.close()
+
 
 
 UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress,UDPRequestHandler)
