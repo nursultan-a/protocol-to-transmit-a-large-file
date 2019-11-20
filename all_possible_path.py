75374b35ea587a66ba228d4c7d7dae2e132bb536
e1906841 <e1906841@r2.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-20 10:26:37 -0800

    all possible ways from source to destination is found

--- all_possible_path.py
+++ all_possible_path.py
@@ -58,15 +58,25 @@ class Graph:
 
 
 # Create a graph given in the above diagram 
-g = Graph(4) 
-g.addEdge(0, 1) 
-g.addEdge(0, 2) 
+g = Graph(5) 
 g.addEdge(0, 3) 
+g.addEdge(0, 2) 
+g.addEdge(0, 1) 
+g.addEdge(4, 3) 
+g.addEdge(4, 2) 
+g.addEdge(4, 1) 
+g.addEdge(3, 0) 
+g.addEdge(3, 2) 
+g.addEdge(3, 4) 
+g.addEdge(1, 0) 
+g.addEdge(1, 2) 
+g.addEdge(1, 4) 
 g.addEdge(2, 0) 
 g.addEdge(2, 1) 
-g.addEdge(1, 3) 
+g.addEdge(2, 3) 
+g.addEdge(2, 4) 
 
-s = 2 ; d = 3
-print ("Following are all different paths from %d to %d :" %(s, d)) 
+s = 0 ; d = 4
+print ("all different paths from source to destination") 
 g.printAllPaths(s, d) 
 #This code is contributed by Neelam Yadav 
