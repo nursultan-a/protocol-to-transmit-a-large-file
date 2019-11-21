2f863ac298539378f452138a770156e2b5487518
e1906841 <e1906841@r3.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-20 23:24:14 -0800

    experiment is done

--- experiment.py
+++ experiment.py
@@ -3,7 +3,7 @@ import socket
 
  
 
-localIP     = "127.0.0.1"
+localIP     = ""
 
 localPort   = 20001
 
@@ -11,46 +11,46 @@ bufferSize  = 1024
 
  
 
-msgFromServer       = "Hello UDP Client"
-
-bytesToSend         = str.encode(msgFromServer)
-
- 
+msgFromR3ForD = "[s->r3->d]"
+msgFromR3ForS = "[d->r3->s]"
+condition = 0
 
 # Create a datagram socket
-
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
  
 
 # Bind to address and ip
-
 UDPServerSocket.bind((localIP, localPort))
 
- 
+print("r3 Up and ready to redirect")
 
-print("UDP server up and listening")
-
- 
-
-# Listen for incoming datagrams
-
+# Listen source node incoming datagrams
 while(True):
 
+    if condition == 2:
+        break
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
     message = bytesAddressPair[0]
-
     address = bytesAddressPair[1]
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
+    clientMsg = repr(message)[2:-1]
+    if clientMsg.split("*")[1] == "0":
+        condition += 1
+    print("s->d: "+clientMsg)
 
-   
 
+    #redirect message from source to destination
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) 
+
+    serverAddressPort = ("10.10.7.1", 20001)
+    bytesToSend = (clientMsg+msgFromR3ForD).encode()
+
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msg = repr(str(msgFromServer[0])[2:-1])
+    print("d->s: "+msg+msgFromR3ForS)
     # Sending a reply to client
 
-    UDPServerSocket.sendto(bytesToSend, address)
+    # redirect response from d to s
+    UDPServerSocket.sendto(msg.encode(), address)
