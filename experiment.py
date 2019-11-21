686fbdced6d3b860339ffb7bf0076352005a2eaa
e1906841 <e1906841@d.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-20 23:22:40 -0800

    experiment is done, no need to change

--- experiment.py
+++ experiment.py
@@ -2,54 +2,45 @@ import socket
 
  
 
-localIP     = "127.0.0.1"
-
+localIP     = ""
 localPort   = 20001
-
 bufferSize  = 1024
-
- 
-
-msgFromServer       = "Hello UDP Client"
-
-bytesToSend         = str.encode(msgFromServer)
-
- 
+msgFromServer       = ["Hello Source Destination", "My IP for r3, which you using to send me a message is 10.10.7.1"]
+condition = 0
 
 # Create a datagram socket
-
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
- 
-
 # Bind to address and ip
-
 UDPServerSocket.bind((localIP, localPort))
-
- 
 
 print("UDP server up and listening")
 
- 
-
-# Listen for incoming datagrams
-
 while(True):
 
+    if condition == 2:
+        break
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
     message = bytesAddressPair[0]
 
     address = bytesAddressPair[1]
 
-    clientMsg = "Message from Client:{}".format(message)
+    clientMsg =repr(message)[2:-1]
     clientIP  = "Client IP Address:{}".format(address)
     
-    print(clientMsg)
-    print(clientIP)
+    print("r3->d: "+clientMsg)
+    #print(clientIP)
 
    
 
     # Sending a reply to client
-
+    if "Hello" in clientMsg:
+        bytesToSend = msgFromServer[0].encode()
+    else:
+        bytesToSend = msgFromServer[1].encode()
     UDPServerSocket.sendto(bytesToSend, address)
+
+
+    if clientMsg.split("*")[1] == "0":
+        condition += 1
