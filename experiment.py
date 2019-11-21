58a403c202e31de47a7091f686387c75956f8d88
e1906841 <e1906841@s.termproject435.ch-geni-net.instageni.cenic.net>
2019-11-20 23:19:29 -0800

    experiment done

--- experiment.py
+++ experiment.py
@@ -1,33 +1,40 @@
 import socket
+import time
 
- 
-
-msgFromClient       = "Hello UDP Server"
-
-bytesToSend         = str.encode(msgFromClient)
-
-serverAddressPort   = ("127.0.0.1", 20001)
-
+msgFromClient       = ["Hello Destination Node","What is your IP?"]
+serverAddressPort   = ("10.10.3.2", 20001)
 bufferSize          = 1024
+experiment_number = 1
+total_time = 0
+rtt = 0
 
- 
+
+def get_time():
+    return int(round(time.time() * 1000))
+
 
 # Create a UDP socket at client side
-
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
- 
-
 # Send to server using created UDP socket
+for experiment in range(experiment_number):
+    for msg in msgFromClient:
 
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        experiment_id = experiment_number - experiment - 1
+        bytesToSend   = str.encode(msg+"*"+str(experiment_id)+"*")
+        
+        start_time = get_time()
 
- 
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        end_time = get_time()
 
- 
+        msgFromServer = repr(msgFromServer[0])[2:-1]
+        print("r3->s: "+msgFromServer)
 
-msg = "Message from Server {}".format(msgFromServer[0])
+        total_time = end_time - start_time
 
-print(msg)
+rtt = total_time / float(experiment_number * 2)
+
+print("rtt: "+ str(rtt))
